0187980cb560724985a5e02caee248370eaf1d05
qixiaa <aqixiaa@gmail.com>
2023-07-10 12:45:50 +1000

    change-styled-components

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
+
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
+`;
+const Title = styled.Text`
+  padding: 16px;
+  color: red;
+`;
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    openingHours = true,
+    rating = 4,
+    isClosedTemporarily = true,
+  } = restaurant;
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
+  );
+};
+
